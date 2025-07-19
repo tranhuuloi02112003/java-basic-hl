79810cc0e367b12a569a7e96973fc1fc1f6bd3da
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-07-19 15:38:03 +0700

    update feature lib management

--- ExersiceOOP/src/LibraryManagement/LibraryManager.java
+++ ExersiceOOP/src/LibraryManagement/LibraryManager.java
@@ -32,15 +32,18 @@ public class LibraryManager {
         return false;
     }
 
-    public ArrayList<Book> findByAuthor(String author) {
+    public ArrayList<Book> findByAuthors(String author) {
         ArrayList<Book> result = new ArrayList<>();
-
         for (Book book : bookList) {
             if (book.getAuthor().equalsIgnoreCase(author)) {
                 result.add(book);
             }
         }
+        return result;
+    }
 
+    public ArrayList<Book> sortBookByPublicationYear(ArrayList<Book> books) {
+        ArrayList<Book> result = books;
         for (int i = 0; i < result.size(); i++) {
             for (int j = i + 1; j < result.size(); j++) {
                 if (result.get(i).getPublicationYear() < result.get(j).getPublicationYear()) {
@@ -53,7 +56,13 @@ public class LibraryManager {
         return result;
     }
 
-    public void allBooksBorrowed() {
+    public ArrayList<Book> findByAuthorsAndSortByPublicationYear(String author) {
+        ArrayList<Book> result = findByAuthors(author);
+        sortBookByPublicationYear(result);
+        return result;
+    }
+
+    public void displayBooksBorrowed() {
         for (int i = 0; i < bookList.size(); i++) {
             if (bookList.get(i).isStatus()) {
                 bookList.get(i).displayInfo();
@@ -91,14 +100,14 @@ public class LibraryManager {
         library.displayAllBooks();
 
         String author = "J.K. Rowling";
-        ArrayList<Book> found = library.findByAuthor(author);
+        ArrayList<Book> found = library.findByAuthorsAndSortByPublicationYear(author);
         System.out.println("\nBooks by " + author + ":");
         for (Book b : found) {
             b.displayInfo();
         }
 
-        System.out.println("\nRemoving book with ISBN03");
-        boolean removed = library.removeBook("ISBN03");
+        System.out.println("\nRemoving book with ISBN02");
+        boolean removed = library.removeBook("ISBN02");
         if (removed) {
             System.out.println("Removed successfully.");
         } else {
@@ -108,6 +117,11 @@ public class LibraryManager {
         library.displayAllBooks();
 
         System.out.println("\nBooks borrowed");
-        library.allBooksBorrowed();
+        library.displayBooksBorrowed();
+
+        System.out.println("\nSearch list by author name and sort by publication year:");
+        for (Book book : library.findByAuthorsAndSortByPublicationYear("J.K. Rowling")) {
+            book.displayInfo();
+        }
     }
 }
