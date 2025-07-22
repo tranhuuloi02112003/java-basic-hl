b0162c01a6a48c1aaf9ec09ab623e49e3654061d
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-07-22 22:19:47 +0700

    refector code 2

---
+++ ExerciseStreamAPI/src/ProductManagement/Laptop.java
@@ -0,0 +1,19 @@
+package ProductManagement;
+
+
+public class Laptop extends Product {
+    private int ramGb;
+
+    public int getRamGb() {
+        return ramGb;
+    }
+
+    public void setRamGb(int ramGb) {
+        this.ramGb = ramGb;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop - " + super.toString() + ", Ram gb =" + ramGb;
+    }
+}
---
+++ ExerciseStreamAPI/src/ProductManagement/Phone.java
@@ -0,0 +1,19 @@
+package ProductManagement;
+
+public class Phone extends Product {
+    private boolean isEsimSupport;
+
+    public boolean isEsimSupport() {
+        return isEsimSupport;
+    }
+
+    public void setEsimSupport(boolean esimSupport) {
+        isEsimSupport = esimSupport;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone - " + super.toString() + ", EsimSupport=" + (isEsimSupport ? "true" : "false");
+    }
+}
+
